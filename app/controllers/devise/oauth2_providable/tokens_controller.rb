6d4ed5e53940092d190a6e17bc9f3298862fc62c
Claudio Poli <masterkain@gmail.com>
2014-01-09 14:37:36 +0100

    lint

--- app/controllers/devise/oauth2_providable/tokens_controller.rb
+++ app/controllers/devise/oauth2_providable/tokens_controller.rb
@@ -6,9 +6,11 @@ class Devise::Oauth2Providable::TokensController < Devise::Oauth2Providable::Bas
     @access_token = @refresh_token.access_tokens.create!(:client => oauth2_current_client, :user => current_user)
     render :json => @access_token.token_response
   end
+
   private
+
   def oauth2_current_client
-   env[Devise::Oauth2Providable::CLIENT_ENV_REF]
+    env[Devise::Oauth2Providable::CLIENT_ENV_REF]
   end
   def oauth2_current_refresh_token
     env[Devise::Oauth2Providable::REFRESH_TOKEN_ENV_REF]
